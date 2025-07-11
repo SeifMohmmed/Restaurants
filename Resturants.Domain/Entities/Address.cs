22fd1ebbf492506329bdaedd4a9c761f15ff290b
Seif Mohamed <seif.eissa7797@gmail.com>
2025-07-11 15:28:38 +0300

    Defining Main Entities

---
+++ Resturants.Domain/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Resturants.Domain.Entities;
+public class Address
+{
+    public string? City { get; set; }
+
+    public string? StreetCode { get; set; }
+
+    public string? PostalCode { get; set; }
+
+}
---
+++ Resturants.Domain/Entities/Resturant.cs
@@ -0,0 +1,22 @@
+﻿namespace Resturants.Domain.Entities;
+public class Resturant
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public string Category { get; set; } = default!;
+
+    public bool HasDelivered { get; set; }
+
+    public string? ContactEmail { get; set; }
+
+    public string? ContactNumber { get; set; }
+
+    public Address? Address { get; set; }
+
+    public List<Dish> Dishes { get; set; } = new();
+
+}
