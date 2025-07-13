9565b1deb27441f76554ff0a2a4be8a03383887f
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-13 20:04:56 +0300

    Feature/restaurant retrieval (#7)
    * Add Manual Mapping Method For Restaurant & Dish DTOs
    
    * Add Automapper
    
    * Add CreateRestaurant endpoint to API
    
    * Fix typo in "Restaurants" spelling
    
    * Add Model validation For CreateRestaurantDTO
    
    * Add FluentValidation for CreateRestaurantDTO
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

---
+++ Restaurants.Application/Restaurants/DTOs/Validators/CreateRestaurantDTOValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.DTOs.Validators;
+public class CreateRestaurantDTOValidator : AbstractValidator<CreateRestaurantDTO>
+{
+    private readonly List<string> validCategories = ["Italian", "Mexican", "Japanese", "American", "Indian"];
+
+    public CreateRestaurantDTOValidator()
+    {
+
+        RuleFor(r => r.Name)
+            .Length(3, 100);
+
+        RuleFor(dto => dto.Category)
+                    .Must(validCategories.Contains)
+                    .WithMessage("Invalid category. Please choose from the valid categories.");
+        //.Custom((value, context) =>
+        //{
+        //    var isValidCategory = validCategories.Contains(value);
+        //    if(!isValidCategory)
+        //    {
+        //        context.AddFailure("Category", "Invalid category. Please choose from the valid categories.");
+        //    }
+        //});
+
+        RuleFor(r => r.ContactEmail)
+            .EmailAddress()
+            .WithMessage("Please provide a valid email address");
+
+        RuleFor(r => r.ContactNumber)
+            .NotEmpty()
+            .Matches(@"^\d{10}$")
+            .WithMessage("Invalid phone number.");
+
+        RuleFor(dto => dto.PostalCode)
+           .Matches(@"^\d{2}-\d{3}$")
+           .WithMessage("Please provide a valid postal code (XX-XXX).");
+
+    }
+}
