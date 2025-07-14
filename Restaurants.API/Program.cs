9c97aa5dcd73c929f6c22f9ac02511a5047a2597
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-14 21:12:58 +0300

    Feature/api utilities (#15)
    * Adding Serilog
    
    * Add Logging To a File Option
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,59 +2,53 @@ using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extentions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
-namespace Resturants.API
+namespace Resturants.API;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            builder.Services.AddApplication();
+        builder.Services.AddApplication();
 
-            builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.AddInfrastructure(builder.Configuration);
 
-            builder.Host.UseSerilog((context, configuration) =>
-                configuration
-                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-                    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
-
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
             );
 
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            var scoope = app.Services.CreateScope();
-            var seeder = scoope.ServiceProvider.GetRequiredService<IRestaurantSeeders>();
-            await seeder.Seed();
+        var scoope = app.Services.CreateScope();
+        var seeder = scoope.ServiceProvider.GetRequiredService<IRestaurantSeeders>();
+        await seeder.Seed();
 
-            app.UseSerilogRequestLogging();
-
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+        app.UseSerilogRequestLogging();
 
 
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
