0ddceb7e2380c3406690644d15f456a9a40bffdb
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-14 22:09:49 +0300

    Feature/api utilities (#18)
    * Adding Serilog
    
    * Add Logging To a File Option
    
    * Add Swagger docs
    
    * Add Error Handling Middleware
    
    * Add NotFound Exception
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

--- Restaurants.API/Middlewares/ErrorHandlingMiddleware.cs
+++ Restaurants.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace Restaurants.API.Middlewares;
+﻿using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.API.Middlewares;
 
 public class ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger) : IMiddleware
 {
@@ -8,6 +10,15 @@ public class ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger) : 
         {
             await next.Invoke(context);
         }
+
+        catch (NotFoundException notFound)
+        {
+            context.Response.StatusCode = 404;
+            await context.Response.WriteAsync(notFound.Message);
+
+            logger.LogWarning(notFound.Message);
+        }
+
         catch (Exception ex)
         {
             logger.LogError(ex, ex.Message);
--- Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
@@ -1,21 +1,23 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 public class UpdateRestaurantCommandHandler(ILogger<UpdateRestaurantCommandHandler> logger,
     IMapper mapper,
-    IRestaurantsRepository restaurantsRepository) : IRequestHandler<UpdateRestaurantCommand, bool>
+    IRestaurantsRepository restaurantsRepository) : IRequestHandler<UpdateRestaurantCommand>
 {
-    public async Task<bool> Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
+    public async Task Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Updating restaurant with id: {RestaurantId} with {@UpdatedRestaurant}", request.Id, request);
 
         var restaurant = await restaurantsRepository.GetByIdAsync(request.Id);
 
         if (restaurant is null)
-            return false;
+            throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
 
         mapper.Map(request, restaurant);
 
@@ -24,7 +26,5 @@ public class UpdateRestaurantCommandHandler(ILogger<UpdateRestaurantCommandHandl
         //restaurant.HasDelivery = request.HasDelivery;
 
         await restaurantsRepository.SaveChangesAsync();
-
-        return true;
     }
 }
