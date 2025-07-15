8f14f40cf25f0588c71e6e0fe5f3920959418e2e
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-15 17:07:20 +0300

    Add Create Endpoint For Dish (#20)
    - Add Create Endpoint For Dish
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

---
+++ Restaurants.Application/Dishes/DTOs/Command/CreateDish/CreateDishCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Dishes.DTOs.Command.CreateDish;
+public class CreateDishCommandValidator : AbstractValidator<CreateDishCommand>
+{
+    public CreateDishCommandValidator()
+    {
+        RuleFor(c => c.Price)
+             .GreaterThanOrEqualTo(0)
+             .WithMessage("Price must be a non-negative number.");
+
+        RuleFor(c => c.KiloCalories)
+             .GreaterThanOrEqualTo(0)
+             .WithMessage("KiloCalories must be a non-negative number.");
+
+
+    }
+}
