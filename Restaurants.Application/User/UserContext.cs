e18658cc21e35c117c579ab9e1c82fa0d08ea1a2
Seif Mohamed <seif.eissa7797@gmail.com>
2025-07-16 17:33:48 +0300

    Add UserContext for accessing authenticated user info

---
+++ Restaurants.Application/User/UserContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Restaurants.Application.User;
+
+/// <summary>
+/// Provides an abstraction for retrieving the currently authenticated user.
+/// </summary>
+public interface IUserContext
+{
+    CurrentUser? GetCurrentUser();
+}
+
+/// <summary>
+/// Allows injection of the user context into any handler or service within the application.
+/// It retrieves the user context information containing values for the user ID claim,
+/// email claim, and the roles of a given user.
+/// </summary>
+/// <param name="httpContextAccessor">
+/// Provides access to the current <see cref="HttpContext"/> to extract user claims.
+/// </param>
+public class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public CurrentUser? GetCurrentUser()
+    {
+        // Retrieves the current authenticated user's context information from the HTTP context.
+        var user = httpContextAccessor?.HttpContext?.User;
+
+        if (user is null)
+            throw new InvalidOperationException("User context is not present");
+
+        if (user.Identity is null || !user.Identity.IsAuthenticated)
+            return null;
+
+        var userId = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+        var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
+        var roles = user.Claims.Where(c => c.Type == ClaimTypes.Role)!.Select(c => c.Value);
+
+        return new CurrentUser(userId, email, roles);
+
+    }
+}
