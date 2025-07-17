2b4eb7f6ae13524a717a80b531582211dab133af
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-17 23:17:38 +0300

    Authorization (#30)
    * Add Role based Authorization
    
    * Assigning User Roles
    
    * Unassigning User Role Endpoint
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

---
+++ Restaurants.Application/Users/Command/UnassignUserRole/UnassignUserRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Restaurants.Application.Users.Command.UnassignUserRole;
+public class UnassignUserRoleCommand : IRequest
+{
+    public string UserEmail { get; set; } = default!;
+
+    public string RoleName { get; set; } = default!;
+
+}
