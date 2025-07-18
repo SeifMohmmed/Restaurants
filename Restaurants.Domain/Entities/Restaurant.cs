36e509a6c77eb47767fb958bbee45850cb3103f6
Seif Mohamed <seif.eissa7797@gmail.com>
2025-07-18 22:31:00 +0300

    Add OwnerId column to Restaurants table with foreign key to AspNetUsers

--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -19,4 +19,8 @@ public class Restaurant
 
     public List<Dish> Dishes { get; set; } = new();
 
+    public ApplicationUser Owner { get; set; } = default!;
+
+    public string OwnerId { get; set; } = default!;
+
 }
