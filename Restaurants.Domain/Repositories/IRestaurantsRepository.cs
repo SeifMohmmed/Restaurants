1a757e201d982dc16aa392ff881d81ff73dee97d
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-20 16:45:23 +0300

    Feature/add pagination (#38)
    * Add Pagination-based Filtering
    
    * Add PagedResult class for structured pagination of generic data collections
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

--- Restaurants.Domain/Repositories/IRestaurantsRepository.cs
+++ Restaurants.Domain/Repositories/IRestaurantsRepository.cs
@@ -4,7 +4,7 @@ namespace Restaurants.Domain.Repositories;
 public interface IRestaurantsRepository
 {
     Task<IEnumerable<Restaurant>> GetAllAsync();
-    Task<IEnumerable<Restaurant>> GetAllMathchingAsync(string? searchPhrase);
+    Task<(IEnumerable<Restaurant>, int)> GetAllMathchingAsync(string? searchPhrase, int pageNumber, int pageSize);
     Task<Restaurant?> GetByIdAsync(int id);
     Task<int> CreateAsync(Restaurant entity);
     Task DeleteAsync(Restaurant entity);
