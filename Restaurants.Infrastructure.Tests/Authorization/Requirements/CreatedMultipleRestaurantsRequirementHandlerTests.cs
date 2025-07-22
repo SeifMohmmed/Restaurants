101e73016237571ee9b38a2d640fe41bb47077f3
Seif Mohamed <143728630+SeifMohmmed@users.noreply.github.com>
2025-07-22 15:03:44 +0300

    Feature/automated testing setup (#47)
    * Add Current user tests
    
    * Add UserContextTests for GetCurrentUser method
    
    * Add CreateRestaurantCommandValidatorTests
    
    * Add RestaurantsProfileTests
    
    * Add CreateRestaurantCommandHandlerTests
    
    * Add UpdateRestaurantCommandHandlerTests
    
    * Add CreatedMultipleRestaurantsRequirementHandlerTests with tests for successful and failed authorization based on restaurant creation count
    
    ---------
    
    Co-authored-by: Seif Mohamed <seif.eissa7797@gmail.com>

---
+++ Restaurants.Infrastructure.Tests/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandlerTests.cs
@@ -0,0 +1,104 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using Moq;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Xunit;
+namespace Restaurants.Infrastructure.Authorization.Requirements.Tests;
+
+public class CreatedMultipleRestaurantsRequirementHandlerTests
+{
+    [Fact()]
+    public async Task HandleRequirementAsync_UserHasCreatedMultipleRestaurants_ShouldSucceed()
+    {
+        //arrange
+
+        var currentUser = new CurrentUser("1", "test@test.com", null, null, null);
+
+        var userContextMock = new Mock<IUserContext>();
+
+        userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+        var restaurants = new List<Restaurant>()
+        {
+            new()
+            {
+                OwnerId=currentUser.Id
+            },
+            new()
+            {
+                OwnerId=currentUser.Id
+            },
+            new()
+            {
+                OwnerId="2"
+            }
+        };
+
+        var restaurantRepositoryMock = new Mock<IRestaurantsRepository>();
+
+        restaurantRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+
+        var requirement = new CreatedMultipleRestaurantsRequirement(2);
+
+        var handler = new CreatedMultipleRestaurantsRequirementHandler
+            (restaurantRepositoryMock.Object,
+            userContextMock.Object);
+
+        var context = new AuthorizationHandlerContext([requirement], null, null);
+
+        //act
+
+        await handler.HandleAsync(context);
+
+        //assert
+        context.HasSucceeded.Should().BeTrue();
+
+    }
+
+    [Fact()]
+    public async Task HandleRequirementAsync_UserHasNotCreatedMultipleRestaurants_ShouldFail()
+    {
+        //arrange
+
+        var currentUser = new CurrentUser("1", "test@test.com", null, null, null);
+
+        var userContextMock = new Mock<IUserContext>();
+
+        userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+        var restaurants = new List<Restaurant>()
+        {
+            new()
+            {
+                OwnerId=currentUser.Id
+            },
+            new()
+            {
+                OwnerId="2"
+            }
+        };
+
+        var restaurantRepositoryMock = new Mock<IRestaurantsRepository>();
+
+        restaurantRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+
+        var requirement = new CreatedMultipleRestaurantsRequirement(2);
+
+        var handler = new CreatedMultipleRestaurantsRequirementHandler
+            (restaurantRepositoryMock.Object,
+            userContextMock.Object);
+
+        var context = new AuthorizationHandlerContext([requirement], null, null);
+
+        //act
+
+        await handler.HandleAsync(context);
+
+        //assert
+        context.HasSucceeded.Should().BeFalse();
+        context.HasFailed.Should().BeTrue();
+
+    }
+}
--- Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
+++ Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
@@ -3,7 +3,7 @@ using Restaurants.Application.Users;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Infrastructure.Authorization.Requirements;
-public class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
+internal class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
     IUserContext userContext) : AuthorizationHandler<CreatedMultipleRestaurantsRequirement>
 {
     protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
